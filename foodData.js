062c6c386e02feb72801b16cd80a45d39153161f
saiabhilekhareddy24-lab <saiabhilekhareddy24@gmail.com>
2026-08-16 19:12:34 +0530

    Create foodData.js

---
+++ foodData.js
@@ -0,0 +1,48 @@
+const restaurants = [
+  {
+    id: 1,
+    name: "Spice Hub",
+    location: "City Center",
+    menu: [
+      {
+        id: 101,
+        name: "Chicken Biryani",
+        price: 180
+      },
+      {
+        id: 102,
+        name: "Paneer Butter Masala",
+        price: 160
+      },
+      {
+        id: 103,
+        name: "Masala Dosa",
+        price: 80
+      }
+    ]
+  },
+  {
+    id: 2,
+    name: "Food Palace",
+    location: "Main Road",
+    menu: [
+      {
+        id: 201,
+        name: "Veg Fried Rice",
+        price: 120
+      },
+      {
+        id: 202,
+        name: "Chicken Noodles",
+        price: 150
+      },
+      {
+        id: 203,
+        name: "Gobi Manchurian",
+        price: 110
+      }
+    ]
+  }
+];
+
+module.exports = restaurants;
